fe67747c1f516a1ce37b87961c17bb3647713db2
jamurillo <jamurillo@grupoautofin.com>
2021-10-23 12:41:29 -0500

    DashBoard

--- CMSEncuestas/src/app/app.component.ts
+++ CMSEncuestas/src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CMSEncuestas';
+  author = 'José Antonio Murillo García';
+  current_Year = new Date().getFullYear();
+  modules = [
+    {
+      name: 'Encuestas', url: 'encuestas',
+      children: [
+        { name: 'GetAll', url: 'getall' },
+        { name: 'Add', url: 'add' },
+      ]
+    },
+    {
+      name: 'Plantillas', url: 'plantillas',
+      children: [
+        { name: 'GetAll', url: 'getall' },
+        { name: 'Add', url: 'add' },
+      ]
+    },
+    {
+      name: 'Reportes', url: 'reportes',
+      children: [
+        { name: 'Get', url: 'get' },
+      ]
+    }
+  ]
 }
